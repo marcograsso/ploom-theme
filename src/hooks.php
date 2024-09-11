01819899eeca0cc7bb38837ef65789255cca3c05
Yaser Ahmady <y@yaserahmady.com>
2024-09-11 18:21:15 +0200

    More

--- src/hooks.php
+++ src/hooks.php
@@ -3,7 +3,14 @@
 add_action("acf/init", "my_acfe_modules");
 function my_acfe_modules()
 {
+    // Enable Classic Editor
     acf_update_setting("acfe/modules/classic_editor", true);
+
+    // Enable Force Sync
+    acf_update_setting("acfe/modules/force_sync", true);
+
+    // Enable Force Sync Deleted Json
+    acf_update_setting("acfe/modules/force_sync/delete", true);
 }
 
 // Fixes site previews when shared on Discord
