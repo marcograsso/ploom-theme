391575abc8419a44eb64079d943216ec8dc23748
Yaser Ahmady <y@yaserahmady.com>
2026-02-03 18:42:35 +0100

    More

--- functions.php
+++ functions.php
@@ -5,11 +5,3 @@ namespace App;
 require_once __DIR__ . "/vendor/autoload.php";
 
 new Bootstrap();
-
-add_action("admin_head", function () {
-    $screen = get_current_screen();
-    if (!$screen) {
-        return;
-    }
-    $screen->remove_help_tabs();
-});
